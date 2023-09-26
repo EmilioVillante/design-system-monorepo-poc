a5430509feb900e342865d3e2af0ad657aeb4fd2
emilio villante <e.villante.griffith@gmail.com>
2023-09-27 08:43:41 +1000

    feat(add-custom-generator): Adds a plugin and custom generator stub

---
+++ tools/my-plugin/src/generators/my-generator/generator.ts
@@ -0,0 +1,32 @@
+import {
+  addProjectConfiguration,
+  formatFiles,
+  generateFiles,
+  Tree,
+} from '@nx/devkit';
+import * as path from 'path';
+import { MyGeneratorGeneratorSchema } from './schema';
+
+export async function myGeneratorGenerator(
+  tree: Tree,
+  options: MyGeneratorGeneratorSchema
+) {
+  const projectRoot = `packages/components/atoms/${options.name}`;
+
+  addProjectConfiguration(tree, options.name, {
+    root: projectRoot,
+    projectType: 'library',
+    sourceRoot: `${projectRoot}/src`,
+    targets: {},
+  });
+
+  const computedOptions = {
+    ...options
+  }
+
+  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, computedOptions);
+
+  await formatFiles(tree);
+}
+
+export default myGeneratorGenerator;
