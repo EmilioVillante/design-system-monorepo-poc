11484b86f939f177fbd385c902c71d84a4a02b0b
emilio villante <e.villante.griffith@gmail.com>
2023-10-03 11:48:11 +1100

    feat(changesets): Updates independent component content

--- libs/components/atoms/example-independent-component/src/lib/example-independent-component.tsx
+++ libs/components/atoms/example-independent-component/src/lib/example-independent-component.tsx
@@ -9,7 +9,7 @@ export function ExampleIndependentComponent(
 ) {
   return (
     <div className={styles['container']}>
-      <h1>We will give this a different name!</h1>
+      <h1>We will give this a different name! TEST</h1>
       <p>Name: {props.name}</p>
     </div>
   );
