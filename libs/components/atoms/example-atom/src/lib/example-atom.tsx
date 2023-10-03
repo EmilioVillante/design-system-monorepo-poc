dc53054c7785a229178ab59f729e771a05a47d31
emilio villante <e.villante.griffith@gmail.com>
2023-10-03 12:29:01 +1100

    feat(changesets): Updates atom content

--- libs/components/atoms/example-atom/src/lib/example-atom.tsx
+++ libs/components/atoms/example-atom/src/lib/example-atom.tsx
@@ -7,7 +7,7 @@ export type ExampleAtomProps = {
 export function ExampleAtom(props: ExampleAtomProps) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to ExampleAtom with change! Test for another changesets</h1>
+      <h1>Welcome to ExampleAtom with change! Test</h1>
       <p>Name: {props.name}</p>
     </div>
   );
