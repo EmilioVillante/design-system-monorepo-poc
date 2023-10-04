ac706b59dfbd34dbd5624f344e0bc3e63b7d6f2d
emilio villante <e.villante.griffith@gmail.com>
2023-10-04 16:58:54 +1100

    docs(storybook): Added story for displaying a package changelog

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -5,7 +5,11 @@ import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
   stories: ['../../../libs/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
--- libs/components/atoms/example-independent-component/README.md
+++
@@ -1,7 +0,0 @@
-# Generic Component
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test ${name of the project}` to execute the unit tests via [Vitest](https://vitest.dev/).
